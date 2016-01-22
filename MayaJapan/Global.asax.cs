b125dc40ec17948aa76b4b79b3acb675ec68e027
Harrison South <Harrison South>
2016-01-22 19:11:28 +0000

    Made several changes

--- MayaJapan/Global.asax.cs
+++ MayaJapan/Global.asax.cs
@@ -16,6 +16,9 @@ namespace MayaJapan
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            ViewEngines.Engines.Clear();
+            ViewEngines.Engines.Add(new RazorViewEngine());
         }
     }
 }
